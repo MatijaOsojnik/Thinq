08ea8582e4e5411597ba261f38c914b1464078fb
Matija Osojnik <matija1.osojnik@gmail.com>
2020-06-14 19:38:31 +0200

    Update 'Browse' Button

--- src/components/Header/User-Header.vue
+++ src/components/Header/User-Header.vue
@@ -12,40 +12,46 @@
       </v-toolbar-title>
 
       <v-menu
-        transition="slide-y-transition"
-        :close-on-content-click="false"
-        v-if="$store.state.isUserLoggedIn"
-      >
-        <template v-slot:activator="{ on }">
-          <v-btn v-on="on" outlined color="#3f3f44" style="margin-left: 30px;">
-            Browse
-            <v-icon small>mdi-menu-down</v-icon>
-          </v-btn>
-        </template>
+      bottom
+      transition="scale-transition"
+      :close-on-content-click="false"
+      v-if="$store.state.isUserLoggedIn"
+    >
+      <template v-slot:activator="{ on }">
+        <v-btn
+          color="#ededed"
+          v-on="on"
+          style="margin-left: 30px;"
+        >
+          Browse
+          <v-icon small>mdi-menu-down</v-icon>
+        </v-btn>
+      </template>
 
-        <v-card max-width="200px">
-          <v-container fluid>
-            <div class="d-flex justify-center align-center flex-column ma-3">
-              <v-btn depressed small block :to="{path: `/lectures`}">All Lectures</v-btn>
-            </div>
-            <v-divider />
-            <span class="subtitle ma-2 d-block font-weight-bold text-center">Categories</span>
-            <div
-              class="d-flex justify-center align-center flex-column ma-1"
-              v-for="category in categories"
-              :key="category.id"
-            >
-              <v-btn
-                depressed
-                small
-                text
-                block
-                :to="{path: `/lectures/categories/${category.id}`}"
-              >{{category.name}}</v-btn>
-            </div>
-          </v-container>
-        </v-card>
-      </v-menu>
+      <v-card max-width="200px">
+        <v-container fluid>
+          <span class="subtitle ma-2 d-block font-weight-bold text-center">Featured</span>
+          <div class="d-flex justify-center align-center flex-column ma-3">
+            <v-btn depressed small text block :to="{name: `lectures`}">All Lectures</v-btn>
+          </div>
+          <v-divider />
+          <span class="subtitle ma-2 d-block font-weight-bold text-center">Categories</span>
+          <div
+            class="d-flex justify-center align-center flex-column ma-1"
+            v-for="category in categories"
+            :key="category.id"
+          >
+            <v-btn
+              depressed
+              small
+              text
+              block
+              :to="{path: `/lectures/categories/${category.id}`}"
+            >{{category.name}}</v-btn>
+          </div>
+        </v-container>
+      </v-card>
+    </v-menu>
 
       <v-spacer></v-spacer>
 

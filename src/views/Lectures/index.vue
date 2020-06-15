3ad6eed925ff9b7da49d50c874a0395c8898abf3
Matija Osojnik <matija1.osojnik@gmail.com>
2020-06-15 18:41:38 +0200

    Add Progress Circular When Loading Lectures

--- src/views/Lectures/index.vue
+++ src/views/Lectures/index.vue
@@ -1,62 +1,78 @@
 <template>
-  <LecturesMetadata>
-    <template v-slot:lectures-panel>
-      <v-container fluid v-if="lectures">
-        <div v-if="$store.state.isUserLoggedIn">
-          <span class="greeting-title" v-if="$store.state.isUserLoggedIn && lectures">
-            Welcome
-            <span class="greeting-name pa-1">{{$store.state.user.display_name}}</span>! Start Your First Class :)
-          </span>
-        </div>
-        <v-container fluid>
-          <span class="title" v-if="$router.history.current['name'] === 'lectures'">All Lectures</span>
-          <span
-            class="title"
-            v-else-if="$router.history.current['name'] === 'lectures-categories' && lectures"
-          >{{lectures[0].Category.name}}</span>
-          <v-row style="z-index: 100" class="flex-sm-fill">
-            <v-col
-              class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-12 d-flex d-sm-flex d-md-block d-lg-block d-xl-block justify-center justify-sm-center"
-              v-if="priviliges"
-            >
-              <LectureCardCreateComponent :card="card" />
-            </v-col>
-            <v-col
-              class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-12 d-flex d-sm-flex d-md-block d-lg-block d-xl-block justify-center justify-sm-center"
-              v-for="lecture in lectureLimit"
-              :key="lecture.id"
-            >
-              <LectureCardComponent :lecture="lecture" />
-            </v-col>
-            <v-col class="col-12 text-center">
-              <v-btn @click="limit = null" v-if="limit && lectures.length > 10">Show More</v-btn>
-            </v-col>
-          </v-row>
-        </v-container>
+  <div>
+    <Header />
+    <div
+      class="d-flex justify-center align-center flex-column fill-height"
+      style="height: 70vh;"
+      v-if="!lectures && !loading"
+    >
+      <span class="display-1 d-block">There are no lectures in this category yet.</span>
+      <v-btn class="ma-4" :to="{name: 'lectures'}">Browse All Lectures</v-btn>
+    </div>
+    <div
+      class="d-flex justify-center align-center flex-column fill-height"
+      height="70vh"
+      v-if="loading"
+    >
+      <v-progress-circular indeterminate color="primary" class="d-block ma-5"></v-progress-circular>
+    </div>
+    <v-container fluid v-if="lectures && !loading">
+      <div v-if="$store.state.isUserLoggedIn">
+        <span class="greeting-title" v-if="$store.state.isUserLoggedIn && lectures">
+          Welcome
+          <span class="greeting-name pa-1">{{$store.state.user.display_name}}</span>! Start Your First Class :)
+        </span>
+      </div>
+      <v-container fluid>
+        <span class="title" v-if="$router.history.current['name'] === 'lectures'">All Lectures</span>
+        <span
+          class="title"
+          v-else-if="$router.history.current['name'] === 'lectures-categories' && lectures"
+        >{{lectures[0].Category.name}}</span>
+        <v-row style="z-index: 100" class="flex-sm-fill">
+          <v-col
+            class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-12 d-flex d-sm-flex d-md-block d-lg-block d-xl-block justify-center justify-sm-center"
+            v-if="priviliges"
+          >
+            <LectureCardCreateComponent :card="card" />
+          </v-col>
+          <v-col
+            class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-12 d-flex d-sm-flex d-md-block d-lg-block d-xl-block justify-center justify-sm-center"
+            v-for="lecture in lectureLimit"
+            :key="lecture.id"
+          >
+            <LectureCardComponent :lecture="lecture" />
+          </v-col>
+          <v-col class="col-12 text-center">
+            <v-btn @click="limit = null" v-if="limit && lectures.length > 10">Show More</v-btn>
+          </v-col>
+        </v-row>
       </v-container>
-    </template>
-  </LecturesMetadata>
+    </v-container>
+  </div>
 </template>
 
 <script>
 import LectureService from "@/services/LectureService.js";
 import LectureCardComponent from "@/components/Card-Lecture";
 import LectureCardCreateComponent from "@/components/Card-Lecture-Create";
-import LecturesMetadata from "@/views/Lectures/Metadata.vue";
+import Header from "@/components/Header/Header.vue";
 export default {
   components: {
-    LecturesMetadata,
     LectureCardComponent,
-    LectureCardCreateComponent
+    LectureCardCreateComponent,
+    Header
   },
   data: () => ({
     card: {
-      imageUrl: "https://images.pexels.com/photos/1762851/pexels-photo-1762851.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-      url: "/lectures/create/",
+      imageUrl:
+        "https://images.pexels.com/photos/1762851/pexels-photo-1762851.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      url: "/lectures/create/"
     },
-      lectures: null,
-      priviliges: false,
-      limit: 10
+    lectures: null,
+    loading: false,
+    priviliges: false,
+    limit: 10
   }),
   computed: {
     lectureLimit() {
@@ -82,17 +98,26 @@ export default {
   },
   methods: {
     async getLectures() {
+      this.loading = true;
       let response = null;
       if (this.$route.params.categoryId) {
         const categoryId = this.$route.params.categoryId;
         response = await LectureService.categories(categoryId);
-        if (response.data.length > 0) {
-          this.lectures = response.data;
+        if (response) {
+          this.loading = false;
+          if (response.data.length > 0) {
+            this.lectures = response.data;
+          } else {
+            this.lectures = null;
+          }
         } else {
-          this.lectures = null;
+          this.loading = false;
         }
       } else {
         response = await LectureService.index();
+        if (response) {
+          this.loading = false;
+        }
         this.lectures = response.data;
       }
     },

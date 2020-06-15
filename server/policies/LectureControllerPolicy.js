b3bfe31aa008f0e4da28c768a3b48a9808b38784
Matija Osojnik <matija1.osojnik@gmail.com>
2020-06-15 10:05:08 +0200

    Add Patern to Lecture Title Field

--- server/policies/LectureControllerPolicy.js
+++ server/policies/LectureControllerPolicy.js
@@ -4,23 +4,24 @@ module.exports = {
     update(req, res, next) {
         const schema = Joi.object({
             title: Joi.string()
-                .alphanum()
+                .pattern(new RegExp('^[A-Za-z0-9 _]*[A-Za-z0-9][A-Za-z0-9 _]*$'))
                 .min(8)
                 .max(30)
                 .messages({
-                    'string.alphanum': 'Display name should only contain alpha-numeric characters',
-                    'string.base': 'Display name should be a type of text',
-                    'string.empty': `Please enter a name`,
+                    'string.pattern.base': 'Title can only contain letters, numbers, underscores and spaces',
+                    'string.base': 'Title should be a type of text',
+                    'string.empty': `Please enter a title`,
                     'string.min': `Title must be longer than 8 characters`,
                     'string.max': `Title name must be shorter than 30 characters`,
-                    'any.required': `Display name is a required field`
+                    'any.required': `Title is a required field`
                 }),
             short_description: Joi.string()
                 .alphanum()
                 .min(20)
                 .max(60)
                 .messages({
-                    'string.base': 'Display name should be a type of text',
+                    'string.alphanum': 'Short description should only contain alpha-numeric characters',
+                    'string.base': 'Short description should be a type of text',
                     'string.empty': `Please enter a short description`,
                     'string.min': `Short description must be at least 20 characters long`,
                     'string.max': `Short description must be maximum 60 characters long`
@@ -29,14 +30,13 @@ module.exports = {
                 .min(50)
                 .max(500)
                 .messages({
-                    'string.pattern.base': `Password must be longer than 8 characters`,
                     'string.min': `Description must be longer than 50 characters`,
                     'string.max': `Display name must be shorter than 300 characters`,
                     'string.empty': `Please write something in the description field`
                 }),
-            thumbnail_url: Joi.string()
-            .allow(''),
-            category_id: Joi.allow('', null) 
+            // thumbnail_url: Joi.string()
+            // .allow(''),
+            // category_id: Joi.allow('', null) 
         }).unknown(true)
 
         const {

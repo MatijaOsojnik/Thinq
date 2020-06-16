dd7635407bd4f81a50a1f72913b02447c22148f2
Matija Osojnik <matija1.osojnik@gmail.com>
2020-06-16 13:59:53 +0200

    No More Free Lecturer Role

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -89,7 +89,7 @@ export default {
           email: this.email,
           password: this.password,
           repeat_password: this.repeat_password,
-          roles: ["Admin"]
+          // roles: [""]
         });
         this.showPanel = true;
 

fd5ea1c14945a776dbc7654f51af0aaa888c3397
Matija Osojnik <matija1.osojnik@gmail.com>
2020-06-16 23:30:48 +0200

    Admin Roles, Categories Loading

--- src/services/GeneralService.js
+++ src/services/GeneralService.js
@@ -3,5 +3,8 @@ import axios from '@/services/axios'
 export default {
     count() {
         return axios.get('admin/general/count',)
+    },
+    getRoles() {
+        return axios.get('roles')
     }
 }
--- src/views/Admin/Categories.vue
+++ src/views/Admin/Categories.vue
@@ -3,26 +3,87 @@
     <AdminHeader />
     <v-container fluid>
       <span class="d-block title pa-2">CATEGORIES</span>
+
+      <v-card>
+        <v-card-title>
+          All Categories
+          <v-spacer></v-spacer>
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search"
+            single-line
+            hide-details
+          ></v-text-field>
+        </v-card-title>
+        <v-data-table
+          v-if="!loading"
+          :loading="loading"
+          loading-text="Loading... Please wait"
+          :headers="headers"
+          :items="statistics"
+          :search="search"
+          class="elevation-1"
+        >
+          <template v-slot:item.actions="{ item }">
+            <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
+            <v-icon small @click="deleteItem(item)">mdi-delete</v-icon>
+          </template>
+        </v-data-table>
+      </v-card>
     </v-container>
   </v-app>
 </template>
 
 <script>
-import GeneralService from "@/services/GeneralService";
-import AdminHeader from '@/components/Header/Admin-Header.vue'
+import CategoryService from "@/services/CategoryService";
+import AdminHeader from "@/components/Header/Admin-Header.vue";
 
 export default {
-    components: {
-        AdminHeader
+  components: {
+    AdminHeader
+  },
+  data: () => ({
+    statistics: null,
+    loading: true,
+    search: "",
+    headers: [
+      {
+        text: "ID",
+        value: "id"
+      },
+      {
+        text: "Name",
+        align: "start",
+        sortable: true,
+        value: "name"
+      },
+      { text: "Description", value: "description" },
+    ],
+    editedItem: {
+      name: "",
+      calories: 0,
+      fat: 0,
+      carbs: 0,
+      protein: 0
     },
+    defaultItem: {
+      name: "",
+      calories: 0,
+      fat: 0,
+      carbs: 0,
+      protein: 0
+    }
+  }),
   created() {
-    this.countEntities();
+    this.getCategories();
   },
   methods: {
-    async countEntities() {
+    async getCategories() {
       try {
-        const response = await GeneralService.count();
+        const response = await CategoryService.index();
         this.statistics = response.data;
+        this.loading = false;
         console.log(response.data);
       } catch (err) {
         console.log(err);

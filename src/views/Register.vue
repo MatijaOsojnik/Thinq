cbfb72a502171f90df636b89ceadadaf93ef7264
Matija Osojnik <matija1.osojnik@gmail.com>
2020-06-17 15:48:09 +0200

    Remove Useless Comment

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -89,7 +89,6 @@ export default {
           email: this.email,
           password: this.password,
           repeat_password: this.repeat_password,
-          // roles: [""]
         });
         this.showPanel = true;
 
